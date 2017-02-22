32504161b795769fd8d9329a3d760f63e2e08113
Noemi del Toro Ayllón <ntoro@ebi.ac.uk>
2017-02-22 10:12:31 +0000

    Fixes some broke tests.

--- jami-bridges/jami-ontology-manager/src/test/java/psidev/psi/mi/jami/bridges/ontologymanager/impl/ols/MIOlsOntologyTest.java
+++ jami-bridges/jami-ontology-manager/src/test/java/psidev/psi/mi/jami/bridges/ontologymanager/impl/ols/MIOlsOntologyTest.java
@@ -1,6 +1,7 @@
 package psidev.psi.mi.jami.bridges.ontologymanager.impl.ols;
 
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 import psidev.psi.mi.jami.bridges.ontologymanager.MIOntologyTermI;
 import psidev.psi.mi.jami.bridges.ontologymanager.impl.OntologyTermWrapper;
@@ -16,6 +17,11 @@ import psidev.psi.tools.ontology_manager.impl.local.OntologyLoaderException;
 
 public class MIOlsOntologyTest {
 
+    /*
+        Multicast problem in ehcache for MAC.
+        -Djava.net.preferIPv4Stack=true needs to be added as a JVM options to be able to run this test in a Mac computer.
+        See http://stackoverflow.com/questions/18747134/getting-cant-assign-requested-address-java-net-socketexception-using-ehcache
+     */
     @Test
     public void test_synonyms() throws OntologyLoaderException {
 
