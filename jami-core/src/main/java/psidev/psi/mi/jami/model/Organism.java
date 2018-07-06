eeb1990e55172e848c157fa742dd65c357a4f4c5
ntoro <ntoro@ebi.ac.uk>
2018-07-06 11:06:16 +0100

    Fixes a typo

--- jami-core/src/main/java/psidev/psi/mi/jami/model/Organism.java
+++ jami-core/src/main/java/psidev/psi/mi/jami/model/Organism.java
@@ -4,7 +4,7 @@ import java.util.Collection;
 
 /**
  * The organism is defined by a taxonomy identifier.
- * It can be used to decsribe the source organism of an interactor, the organism in which the experiment has been performed or
+ * It can be used to describe the source organism of an interactor, the organism in which the experiment has been performed or
  * the organism where the participants of an interaction have been expressed in.
  *
  * @author Marine Dumousseau (marine@ebi.ac.uk)
