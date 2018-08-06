50149c09350c7e2003f7dadabdd812f9e07431e1
ntoro <ntoro@ebi.ac.uk>
2018-08-06 18:12:04 +0100

    jami-europubmedcentral: updates the code to use Java Logging instead of an external library when possible. Review the logging levels too.

--- jami-bridges/jami-europubmedcentral/src/test/java/psidev/psi/mi/jami/bridges/europubmedcentral/CachedEuroPubmedCentralFetcherTest.java
+++ jami-bridges/jami-europubmedcentral/src/test/java/psidev/psi/mi/jami/bridges/europubmedcentral/CachedEuroPubmedCentralFetcherTest.java
@@ -2,11 +2,11 @@ package psidev.psi.mi.jami.bridges.europubmedcentral;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import psidev.psi.mi.jami.bridges.exception.BridgeFailedException;
 import psidev.psi.mi.jami.model.Publication;
 import psidev.psi.mi.jami.model.Xref;
+
+import java.util.logging.Logger;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -19,7 +19,7 @@ import static org.junit.Assert.assertTrue;
  */
 public class CachedEuroPubmedCentralFetcherTest {
 
-    protected static final Logger log = LoggerFactory.getLogger(EuroPubmedCentralFetcherTest.class.getName());
+    protected static final Logger log = Logger.getLogger(EuroPubmedCentralFetcherTest.class.getName());
 
     EuroPubmedCentralFetcher fetcher;
 
