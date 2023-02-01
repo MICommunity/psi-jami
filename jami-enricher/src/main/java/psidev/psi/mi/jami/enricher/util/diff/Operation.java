5ddcdb23d80cda220d0b09d0ca9aa1b34ca61ff5
jmedinaebi <jmedina@ebi.ac.uk>
2023-02-01 13:33:09 +0000

    Reformat Java code

--- jami-enricher/src/main/java/psidev/psi/mi/jami/enricher/util/diff/Operation.java
+++ jami-enricher/src/main/java/psidev/psi/mi/jami/enricher/util/diff/Operation.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2008 The European Bioinformatics Institute, and others.
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,15 +15,15 @@
  */
 package psidev.psi.mi.jami.enricher.util.diff;
 
-  /**
-   *-
-   * The data structure representing a diff is a Linked list of Diff objects:
-   * {Diff(Operation.DELETE, "Hello"), Diff(Operation.INSERT, "Goodbye"),
-   *  Diff(Operation.EQUAL, " world.")}
-   * which means: delete "Hello", add "Goodbye" and keep " world."
-   *
-   */
-  public enum Operation {
+/**
+ *-
+ * The data structure representing a diff is a Linked list of Diff objects:
+ * {Diff(Operation.DELETE, "Hello"), Diff(Operation.INSERT, "Goodbye"),
+ *  Diff(Operation.EQUAL, " world.")}
+ * which means: delete "Hello", add "Goodbye" and keep " world."
+ *
+ */
+public enum Operation {
     DELETE(diff_match_patch.Operation.DELETE),
     INSERT(diff_match_patch.Operation.INSERT),
     EQUAL(diff_match_patch.Operation.EQUAL);
